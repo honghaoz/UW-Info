11919938c1f693fddb3ce6a611ea6bf0880c0fcc
Zefeng Qiu <z9qiu@uwaterloo.ca>
2015-08-26 17:20:26 -0400

    add loading cell and singleton model to get the parsed html data

---
+++ UW Info Session 2.0/UW Info Session/UW Info Session/Component/List/Views/LoadingCell.swift
@@ -0,0 +1,24 @@
+//
+//  ParsingTableViewCell.swift
+//  UW Info Session
+//
+//  Created by Qiu Zefeng on 2015-08-26.
+//  Copyright (c) 2015 Honghao Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ParsingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ UW Info Session 2.0/UW Info Session/UW Info Session/Model/Info.swift
@@ -0,0 +1,9 @@
+//
+//  Info.swift
+//  UW Info Session
+//
+//  Created by Qiu Zefeng on 2015-08-25.
+//  Copyright (c) 2015 Honghao Zhang. All rights reserved.
+//
+
+import Foundation
