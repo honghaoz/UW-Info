f78587cc4790c19c834122804873c5dc3fe51daa
Zefeng Qiu <z9qiu@uwaterloo.ca>
2015-08-27 22:23:47 -0400

    show the parsing result in the list view controller

---
+++ UW Info Session 2.0/UW Info Session/UW Info Session/Component/Detail/BaseInfoCell.swift
@@ -0,0 +1,24 @@
+//
+//  BaseInfoCell.swift
+//  UW Info Session
+//
+//  Created by Qiu Zefeng on 2015-08-27.
+//  Copyright (c) 2015 Honghao Zhang. All rights reserved.
+//
+
+import UIKit
+
+class BaseInfoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ UW Info Session 2.0/UW Info Session/UW Info Session/Protocol/ViewConnector.swift
@@ -0,0 +1,9 @@
+//
+//  ViewConnector.swift
+//  UW Info Session
+//
+//  Created by Qiu Zefeng on 2015-08-27.
+//  Copyright (c) 2015 Honghao Zhang. All rights reserved.
+//
+
+import Foundation
